6266c6a483c15483a5e81116181d8552323b6f5b
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-12 18:04:41 +0100

    fix(Unit Test): syntax error

--- src/providers/radio-service/radio-service.spec.ts
+++ src/providers/radio-service/radio-service.spec.ts
@@ -5,7 +5,7 @@ import { Events } from 'ionic-angular';
 import { EventsMock } from 'ionic-mocks';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { GlobalService } from '../global-service/global-service';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs';
 
 describe( 'Provider: RadioService', () => {
 
@@ -79,7 +79,7 @@ describe( 'Provider: RadioService', () => {
     it( 'getApiSongs requests API', ( done ) => {
         const spy = spyOn( service, 'getApiSongs' ).and.callThrough();
         spyOn( service.http, 'get' ).and.returnValue( Observable.of( {} ) );
-        spyOn( service, 'filterDefaultCovers' ).and.callFake( () => { return {}; } );
+        spyOn( service, 'filterDefaultCovers' ).and.callFake( () => {} );
         spyOn( service, 'onGetApiSongsSuccess' ).and.callFake( () => {} );
         spyOn( service, 'onGetApiSongsError' ).and.callFake( () => {} );
 
@@ -147,46 +147,46 @@ describe( 'Provider: RadioService', () => {
 
     it( 'onGetApiSongsSuccess does nothing when current song hasn\'nt changed', () => {
         const songs = {songs: [
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                    title: 'The Beatles - Drive My Car'
-                },
-                {
-                    album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                    title: 'Unknown - Sample faites attention'
-                },
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                    title: 'The Doors - Love Me Two Times'
-                },
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                    title: 'Jef Barbara - Theme From Our Weekend'
-                },
-                {
-                    album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                    title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
-                },
-                {
-                    album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                    title: 'Unknown - Sample train'
-                },
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                    title: 'August Albert - Chagrin d Amour'
-                },
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                    title: 'The Seasons - The Way It Goes'
-                },
-                {
-                    album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
-                    title: 'Flow Dynamics - Live In the Mix'
-                },
-                {
-                    album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                    title: 'Unknown - sample yolalaitou'
-                } ] };
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+                title: 'The Beatles - Drive My Car'
+            },
+            {
+                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+                title: 'Unknown - Sample faites attention'
+            },
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+                title: 'The Doors - Love Me Two Times'
+            },
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+                title: 'Jef Barbara - Theme From Our Weekend'
+            },
+            {
+                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+                title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
+            },
+            {
+                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+                title: 'Unknown - Sample train'
+            },
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+                title: 'August Albert - Chagrin d Amour'
+            },
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+                title: 'The Seasons - The Way It Goes'
+            },
+            {
+                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
+                title: 'Flow Dynamics - Live In the Mix'
+            },
+            {
+                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+                title: 'Unknown - sample yolalaitou'
+            } ] };
         service.currentSong = songs.songs[ 0 ];
         service.events = new EventsMock();
         service.events.publish = () => {};
