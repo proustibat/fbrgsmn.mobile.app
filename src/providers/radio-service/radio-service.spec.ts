5aba8a6cc22b4cfe3443c7819f54df5d72e6a635
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-12 18:34:52 +0100

    fix(CodeClimate): issues and config for duplication

--- src/providers/radio-service/radio-service.spec.ts
+++ src/providers/radio-service/radio-service.spec.ts
@@ -13,7 +13,47 @@ describe( 'Provider: RadioService', () => {
     let injector;
     let service;
     let originalTimeout;
-
+    const songs = {songs: [
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+            title: 'The Beatles - Drive My Car'
+        },
+        {
+            album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+            title: 'Unknown - Sample faites attention'
+        },
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+            title: 'The Doors - Love Me Two Times'
+        },
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+            title: 'Jef Barbara - Theme From Our Weekend'
+        },
+        {
+            album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+            title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
+        },
+        {
+            album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+            title: 'Unknown - Sample train'
+        },
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+            title: 'August Albert - Chagrin d Amour'
+        },
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
+            title: 'The Seasons - The Way It Goes'
+        },
+        {
+            album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
+            title: 'Flow Dynamics - Live In the Mix'
+        },
+        {
+            album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
+            title: 'Unknown - sample yolalaitou'
+        } ] };
     const moduleDef = {
         declarations: [
 
@@ -92,47 +132,6 @@ describe( 'Provider: RadioService', () => {
     } );
 
     it( 'onGetApiSongsSuccess publishes an event when current song has changed', () => {
-        const songs = {songs: [
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Beatles - Drive My Car'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample faites attention'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Doors - Love Me Two Times'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'Jef Barbara - Theme From Our Weekend'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample train'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'August Albert - Chagrin d Amour'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Seasons - The Way It Goes'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
-                title: 'Flow Dynamics - Live In the Mix'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - sample yolalaitou'
-            } ] };
         service.events = new EventsMock();
         service.events.publish = () => {};
         spyOn( service.events, 'publish' ).and.callFake( () => [] );
@@ -146,47 +145,6 @@ describe( 'Provider: RadioService', () => {
     } );
 
     it( 'onGetApiSongsSuccess does nothing when current song hasn\'nt changed', () => {
-        const songs = {songs: [
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Beatles - Drive My Car'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample faites attention'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Doors - Love Me Two Times'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'Jef Barbara - Theme From Our Weekend'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample train'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'August Albert - Chagrin d Amour'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Seasons - The Way It Goes'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
-                title: 'Flow Dynamics - Live In the Mix'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - sample yolalaitou'
-            } ] };
         service.currentSong = songs.songs[ 0 ];
         service.events = new EventsMock();
         service.events.publish = () => {};
@@ -209,47 +167,6 @@ describe( 'Provider: RadioService', () => {
     } );
 
     it( 'filterDefaultCovers', () => {
-        const songs = {songs: [
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Beatles - Drive My Car'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample faites attention'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Doors - Love Me Two Times'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'Jef Barbara - Theme From Our Weekend'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Faubourg Simone - Flash Calepin Obsession Marlene'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - Sample train'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'August Albert - Chagrin d Amour'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/72ed10a859fb4c1fb29a546078ec737d.png',
-                title: 'The Seasons - The Way It Goes'
-            },
-            {
-                album_cover: 'https://lastfm-img2.akamaized.net/i/u/300x300/6a20981e0f4df4fbce694890a976bd8b.png',
-                title: 'Flow Dynamics - Live In the Mix'
-            },
-            {
-                album_cover: 'http://faubourgsimone.com/player/medias/imgs/pochette-default.jpg',
-                title: 'Unknown - sample yolalaitou'
-            } ] };
         const data = service.filterDefaultCovers( songs );
         expect( typeof( data ) ).toEqual( 'object' );
     } );
