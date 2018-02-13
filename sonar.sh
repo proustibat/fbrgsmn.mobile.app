a00f506173b6a1a07fda5c27f23288812188f19f
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-13 16:23:02 +0100

    Develop (#10)
    * ci(SonarScan): on all branches with sonar.branch.name property
    
    * ci(SonarScan): all branches with regex in Travis config
    
    * ci(SonarScan): add develop branch in Travis config
    
    * Sonarcloud: plugin branch not available, use sonar.branch (new project dashboard)
    
    * Sonarcloud: travis config
    
    * ci(SonarScan): add a script for local sonar-scan
    
    Useful to run on local for branch different than master
    
    * Update README.md

--- sonar.sh
+++ sonar.sh
@@ -24,5 +24,5 @@ function readJson {
 
 VERSION=`readJson package.json version` || exit 1;
 
-sonar-scanner -X -Dsonar.projectVersion=$VERSION
+sonar-scanner -X -Dsonar.projectVersion=$VERSION -Dsonar.branch=$TRAVIS_BRANCH
 
